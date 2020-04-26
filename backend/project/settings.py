2ffa46474f3e8a98d333bcb02a1072510702b91d
Joelson Antônio dos Santos <joelson.santos@usp.br>
2020-04-26 15:49:06 -0300

    Swagger  - dev mode do Django.

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'project.login',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,3 +1,7 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
@@ -23,7 +27,23 @@ class UserViewSet(viewsets.ModelViewSet):
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MESALVE - APIs",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view())
 ]
